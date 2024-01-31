ebbc1db73f4450cb68564593c39f643e43cb13a7
Nyakio-Eva <evalkariuki@gmail.com>
2024-01-31 16:24:25 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About(){
+    return (
+    <div>
+        <h2 id="about">About</h2>
+    </div>
+  );
+}
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,8 @@
 import React from "react";
+import Home from "./Home.js";
+import NavBar from "./Navbar.js";
+import About from "./About.js";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
 {/* write an <About> component here */}
 
@@ -23,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
